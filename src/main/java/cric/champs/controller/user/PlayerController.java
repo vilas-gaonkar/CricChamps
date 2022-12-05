169d00d42607e63ba203507cb2f179c4b8b74863
Vilas_05 <vilas.gaonkar@robosoftin.com>
2022-12-05 18:13:33 +0530

    Team and player controller

---
+++ src/main/java/cric/champs/controller/user/PlayerController.java
@@ -0,0 +1,82 @@
+package cric.champs.controller.user;
+
+import com.cloudinary.utils.ObjectUtils;
+import cric.champs.entity.Players;
+import cric.champs.entity.ResultModel;
+import cric.champs.service.cloud.UploadImageTOCloud;
+import cric.champs.service.user.PlayerInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/player")
+public class PlayerController {
+    @Autowired
+    private UploadImageTOCloud uploadImageTOCloud;
+
+    @Autowired
+    private PlayerInterface playerInterface;
+
+    @PostMapping("/register")
+    public ResponseEntity<ResultModel> register(@ModelAttribute Players player, @RequestPart MultipartFile playerPhoto) throws IOException {
+        Map result = null;
+        if (playerPhoto == null)
+            player.setProfilePhoto(null);
+        else if (playerPhoto.isEmpty())
+            player.setProfilePhoto(null);
+        else
+            result = uploadImageTOCloud.uploadImage(playerPhoto.getBytes(), ObjectUtils.asMap("resource type", "auto"));
+
+        if (result != null)
+            player.setProfilePhoto(result.get("url").toString());
+
+        try {
+            return ResponseEntity.of(Optional.of(playerInterface.registerPlayer(player)));
+        } catch (Exception exception) {
+            return ResponseEntity.ok(new ResultModel(exception.getMessage()));
+        }
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<ResultModel> edit(@ModelAttribute Players player, @RequestPart MultipartFile playerPhoto) throws IOException {
+        Map result = null;
+        if (playerPhoto == null)
+            player.setProfilePhoto(null);
+        else if (playerPhoto.isEmpty())
+            player.setProfilePhoto(null);
+        else
+            result = uploadImageTOCloud.uploadImage(playerPhoto.getBytes(), ObjectUtils.asMap("resource type", "auto"));
+
+        if (result != null)
+            player.setProfilePhoto(result.get("url").toString());
+
+        try {
+            return ResponseEntity.of(Optional.of(playerInterface.editPlayer(player)));
+        } catch (Exception exception) {
+            return ResponseEntity.ok(new ResultModel(exception.getMessage()));
+        }
+    }
+
+    @DeleteMapping("/remove")
+    public ResponseEntity<ResultModel> delete(@RequestPart long playerId, @RequestPart long teamId, @RequestPart long tournamentId) {
+        return ResponseEntity.of(Optional.of(playerInterface.deletePlayer(playerId, teamId, tournamentId)));
+    }
+
+    @GetMapping("/view")
+    public ResponseEntity<?> getAllPlayers(@RequestPart long teamId, @RequestPart long tournamentId, @RequestParam int pageSize, @RequestParam int pageNumber) {
+
+        return ResponseEntity.of(Optional.of(playerInterface.getAllPlayers(teamId, tournamentId, pageSize, pageNumber)));
+    }
+
+    @GetMapping("/view/player")
+    public ResponseEntity<?> getPlayer(@RequestPart long playerId, @RequestPart long teamId, @RequestPart long tournamentId) {
+
+        return ResponseEntity.of(Optional.of(playerInterface.getPlayer(playerId, teamId, tournamentId)));
+    }
+}
---
+++ src/main/java/cric/champs/controller/user/TeamController.java
@@ -0,0 +1,85 @@
+package cric.champs.controller.user;
+
+import com.cloudinary.utils.ObjectUtils;
+import cric.champs.entity.ResultModel;
+import cric.champs.entity.Teams;
+import cric.champs.service.cloud.UploadImageTOCloud;
+import cric.champs.service.user.TeamInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/team")
+public class TeamController {
+
+    @Autowired
+    private UploadImageTOCloud uploadImageTOCloud;
+
+    @Autowired
+    private TeamInterface teamInterface;
+
+    @PostMapping("/register")
+    public ResponseEntity<ResultModel> register(@ModelAttribute Teams team, @RequestPart MultipartFile teamPhoto) throws IOException {
+        Map result = null;
+        if (teamPhoto == null)
+            team.setTeamLogo(null);
+        else if (teamPhoto.isEmpty())
+            team.setTeamLogo(null);
+        else
+            result = uploadImageTOCloud.uploadImage(teamPhoto.getBytes(), ObjectUtils.asMap("resource type", "auto"));
+
+        if (result != null)
+            team.setTeamLogo(result.get("url").toString());
+
+        try {
+            return ResponseEntity.of(Optional.of(teamInterface.registerTeam(team)));
+        } catch (Exception exception) {
+            return ResponseEntity.ok(new ResultModel(exception.getMessage()));
+        }
+    }
+
+    @PutMapping("/edit")
+    public ResponseEntity<ResultModel> edit(@ModelAttribute Teams team, @RequestPart MultipartFile teamPhoto) throws IOException {
+        Map result = null;
+        if (teamPhoto == null)
+            team.setTeamLogo(null);
+        else if (teamPhoto.isEmpty())
+            team.setTeamLogo(null);
+        else
+            result = uploadImageTOCloud.uploadImage(teamPhoto.getBytes(), ObjectUtils.asMap("resource type", "auto"));
+
+        if (result != null)
+            team.setTeamLogo(result.get("url").toString());
+
+        try {
+            return ResponseEntity.of(Optional.of(teamInterface.editTeam(team)));
+        } catch (Exception exception) {
+            return ResponseEntity.ok(new ResultModel(exception.getMessage()));
+        }
+    }
+
+    @DeleteMapping("/remove")
+    public ResponseEntity<ResultModel> delete(@RequestPart long teamId, @RequestPart long tournamentId) {
+        return ResponseEntity.of(Optional.of(teamInterface.deleteTeam(teamId, tournamentId)));
+    }
+
+    @GetMapping("/view")
+    public ResponseEntity<?> getAllTeams(@RequestPart long tournamentId, @RequestParam int pageSize, @RequestParam int pageNumber) {
+
+        return ResponseEntity.of(Optional.of(teamInterface.getAllTeams(tournamentId, pageSize, pageNumber)));
+    }
+
+    @GetMapping("/view/team")
+    public ResponseEntity<?> getTeam(@RequestPart long teamId, @RequestPart long tournamentId) {
+
+        return ResponseEntity.of(Optional.of(teamInterface.getTeam(teamId, tournamentId)));
+    }
+
+
+}
