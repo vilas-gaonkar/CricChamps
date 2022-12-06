945c55edcd4efc148d1f1cb248370aecad9b5df6
Siddhesh <siddhesh.sawant@robosoftin.com>
2022-12-06 10:04:14 +0530

    Added Matches Class

---
+++ src/main/java/cric/champs/entity/Matches.java
@@ -0,0 +1,46 @@
+package cric.champs.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+import java.sql.Time;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Matches {
+
+    private long matchId;
+
+    private long tournamentId;
+
+    private long groundId;
+
+    private String groundName;
+
+    private int roundNumber;
+
+    private int matchNumber;
+
+    private String matchStatus;
+
+    private String statusDescription;
+
+    private Date matchDate;
+
+    private Date matchDay;
+
+    private Time matchStartTime;
+
+    private Time matchEndTime;
+
+    private String isCancelled;
+
+    private String cancelledReason;
+
+}
