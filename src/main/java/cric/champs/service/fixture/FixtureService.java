61516fd0897d813baa2f0734feb8d23d299f614e
ChandanGitHubAccount <chandan.hu@robosoftin.com>
2022-12-08 18:07:01 +0530

    fixture service

--- src/main/java/cric/champs/service/fixture/FixtureService.java
+++ src/main/java/cric/champs/service/fixture/FixtureService.java
@@ -104,17 +104,13 @@ public class FixtureService implements FixtureGenerationInterface {
 
     private boolean roundRobinGenerationForKnockout(long[] teamsId, Tournaments tournament) {
         int numberOfTeams = teamsId.length;
-        long byeTeamId=0;
-        int matchNumber=1;
+        long byeTeamId = 0;
+        int matchNumber = 1;
         LocalTime startTime = tournament.getTournamentStartTime().toLocalTime();
         LocalDate startDate = tournament.getTournamentStartDate();
         LocalTime endTime = tournament.getTournamentEndTime().toLocalTime();
-
-
-        if (numberOfTeams % 2 == 1) {
+        if (numberOfTeams % 2 == 1)
             byeTeamId = teamsId[teamsId.length - 1];
-        }
-
         for (int teamIdIndex = 0; teamIdIndex < teamsId.length / 2; teamIdIndex++) {
             LocalTime inningEndTime = getEndTime(tournament, startTime);
             if (endTime.isBefore(startTime.plusHours(inningEndTime.getHour()))) {
@@ -127,9 +123,7 @@ public class FixtureService implements FixtureGenerationInterface {
 
             startTime = startTime.plusHours(inningEndTime.getHour());
             matchNumber++;
-
         }
-
         return false;
     }
 
@@ -170,7 +164,7 @@ public class FixtureService implements FixtureGenerationInterface {
         int matchPerGround = matches.size() / tournament.getNumberOfGrounds();
         int remainingMatches = matches.size() - tournament.getNumberOfGrounds() * matchPerGround;
         assignGroundsToLeague(grounds, matches, matchPerGround, remainingMatches);
-        assignUmpiresToAllLeagueMatches(umpires, matches,tournament);
+        assignUmpiresToAllLeagueMatches(umpires, matches, tournament);
     }
 
     private void assignUmpiresToAllLeagueMatches(List<Umpires> umpires, List<Matches> matches, Tournaments tournament) {
@@ -315,7 +309,7 @@ public class FixtureService implements FixtureGenerationInterface {
     }
 
     private Matches insertIntoMatchesOfLeague(long tournamentId, int round, int matchNumber, LocalTime startTime, LocalTime endTime, LocalDate matchDate) {
-        jdbcTemplate.update("insert into matches values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", null, tournamentId, null, null,null,null,
+        jdbcTemplate.update("insert into matches values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", null, tournamentId, null, null, null, null,
                 round, matchNumber, MatchStatus.UPCOMING.toString(), matchDate, DayOfWeek.from(matchDate).name(), startTime, endTime, "false", null);
         return jdbcTemplate.query("SELECT * FROM matches ORDER BY matchId DESC LIMIT 1",
                 new BeanPropertyRowMapper<>(Matches.class)).get(0);
