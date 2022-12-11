75b47429c3ca297f9a1adf6d99b9ddf44576fc22
ChandanGitHubAccount <chandan.hu@robosoftin.com>
2022-12-11 12:45:38 +0530

    scoreboard functions

--- src/main/java/cric/champs/service/scoreboardandlivescore/ScoreBoardService.java
+++ src/main/java/cric/champs/service/scoreboardandlivescore/ScoreBoardService.java
@@ -11,9 +11,11 @@ import cric.champs.service.system.SystemInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ScoreBoardService implements ScoreboardInterface {
 
     @Autowired
