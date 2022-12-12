2a8975d58a1f89455142a969b14bc8d45c58bf06
ChandanGitHubAccount <chandan.hu@robosoftin.com>
2022-12-12 19:09:44 +0530

    scoreboard updation during live score update

--- src/main/java/cric/champs/service/scoreboardandlivescore/LiveScoreService.java
+++ src/main/java/cric/champs/service/scoreboardandlivescore/LiveScoreService.java
@@ -1,10 +1,7 @@
 package cric.champs.service.scoreboardandlivescore;
 
 import cric.champs.customexceptions.LiveScoreUpdationException;
-import cric.champs.entity.Matches;
-import cric.champs.entity.Players;
-import cric.champs.entity.Teams;
-import cric.champs.entity.Tournaments;
+import cric.champs.entity.*;
 import cric.champs.livescorerequestmodels.LiveScoreUpdate;
 import cric.champs.resultmodels.SuccessResultModel;
 import cric.champs.service.MatchStatus;
@@ -59,26 +56,31 @@ public class LiveScoreService implements LiveInterface {
         if (strikePlayer.isEmpty() || nonStrikePlayer.isEmpty() || bowlingPlayer.isEmpty())
             throw new LiveScoreUpdationException("Invalid player");
         numberOfOversOfTournament = tournament.get(0).getNumberOfOvers();
+        Long scoreBoardId = null;
         if (liveScoreUpdateModel.getOver() == 0 && (liveScoreUpdateModel.getBall() == 1 || (liveScoreUpdateModel.getExtraModel().isExtraStatus() && liveScoreUpdateModel.getBall() == 0)))
-            setStatus(liveScoreUpdateModel.getTournamentId(), liveScoreUpdateModel.getMatchId());
-        updateScoreBoard(tournament.get(0), matches.get(0), nonStrikeTeam, strikeTeam.get(0), liveScoreUpdateModel);
+            scoreBoardId = setStatus(liveScoreUpdateModel.getTournamentId(), liveScoreUpdateModel.getMatchId(), liveScoreUpdateModel.getBattingTeamId(), strikeTeam.get(0).getTeamName());
+        updateScoreBoard(tournament.get(0), matches.get(0), nonStrikeTeam, strikeTeam.get(0), liveScoreUpdateModel,scoreBoardId);
         updateLiveScoreAndCommentry(tournament.get(0), matches.get(0), nonStrikeTeam, strikeTeam.get(0), liveScoreUpdateModel);
         return null;
     }
 
-    private boolean setStatus(Long tournamentId, Long matchId) {
+    private Long setStatus(Long tournamentId, Long matchId, Long teamId, String teamName) {
         jdbcTemplate.update("update matches set matchStatus = ? where matchId = ? and tournamentId = ?", MatchStatus.LIVE.toString(),
                 matchId, tournamentId);
         jdbcTemplate.update("update tournaments set tournamentStatus = ? where tournamentId = ?", TournamentStatus.PROGRESS.toString(),
                 tournamentId);
-        return true;
+        jdbcTemplate.update("insert into scoreBoard values(?,?,?,?,?,?,?,?,?)", null, tournamentId, matchId,
+                teamId, teamName, 0, 0, 0, 0);
+        return jdbcTemplate.query("select * from scoreBoard order by scoreBoardId DESC limit 1",
+                new BeanPropertyRowMapper<>(ScoreBoard.class)).get(0).getScoreBoardId();
     }
 
     private void updateLiveScoreAndCommentry(Tournaments tournaments, Matches matches, List<Teams> matchTeams, Teams teams, LiveScoreUpdate liveScoreUpdateModel) {
     }
 
-    private void updateScoreBoard(Tournaments tournaments, Matches matches, List<Teams> matchTeams, Teams teams, LiveScoreUpdate liveScoreUpdateModel) {
-
+    private void updateScoreBoard(Tournaments tournaments, Matches matches, List<Teams> matchTeams, Teams teams, LiveScoreUpdate liveScoreUpdateModel,Long scoreBoardId) {
+        if (liveScoreUpdateModel.getExtraModel().isExtraStatus())
+            jdbcTemplate.update("insert into scoreBoard values(");
     }
 
     private double getBattingStrikeRate(int runsScored, int numberOfBallFaced) {
